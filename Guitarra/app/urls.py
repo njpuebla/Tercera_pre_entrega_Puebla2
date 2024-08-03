5f2518a7fe7dffcc4938989d71fc4db2ec27cfbe
Nicolás Puebla <njpuebla@gmail.com>
2024-08-03 19:47:44 -0300

    Agregué formularios para ingreso de datos y búsquedas

--- Guitarra/app/urls.py
+++ Guitarra/app/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    path("", views.inicio)
+    path("", views.inicio, name="Inicio"),
+    path('electricas/', views.electricas, name='Electricas'),
+    path('acusticas/', views.acusticas, name='Acusticas'),
+    path('amplificadores/', views.amplificadores, name='Amplificadores'),
+    path('efectos/', views.efectos, name='Efectos'),
+    path('buscar-electricas/', views.buscar_electricas, name="Buscar_Electricas"),
 ]
